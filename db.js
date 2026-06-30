9a6d105fdd41c50b2d5cda32abc74a80e2cd078d
Satya Thakur <satyathakur940@gmail.com>
2026-07-01 00:24:49 +0530

    Added Gitignore

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Link = new Schema({
     originalUrl : String,
-    shortUrl : String
+    shortUrl : {type : String, unique : true}
 })
 
 const linkModel = mongoose.model('main-data', Link);
